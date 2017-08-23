f9e3c838da0c5dff9691447eb422c2656c651827
David Siorpaes <david.siorpaes@st.com>
2017-08-23 18:34:42 +0200

    Added few exercises from 99 questions

--- 99questions.hs
+++ 99questions.hs
@@ -1,4 +1,4 @@
--- https://wiki.haskell.org/99_questions/
+-- https://wiki.haskell.org/99_questions
 
 --Problem 1
 mylast :: [a] -> a
@@ -19,7 +19,28 @@ myButLast xs = xs !! (length xs - 2)
 myButLast' :: [a] -> a
 myButLast' xs = head (drop ((length xs) - 2) xs)
 
+--Problem 3
+elementAt :: [a] -> Int -> a
+elementAt xs n = xs !! (n - 1)
 
+--Problem 4
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (x:xs) = 1 + myLength xs
 
+myLength' :: [a] -> Int
+myLength' xs = foldr (\_ n -> n + 1) 0 xs
 
+--Problem 5
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
 
+--Problem 6
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome xs = xs == myReverse xs
+
+--Problem 8
+compress :: Eq a => [a] -> [a]
+compress [x] = [x]
+compress xs = init (foldr (\x ys -> if x /= head ys then x:ys else ys) [head xs] xs)
